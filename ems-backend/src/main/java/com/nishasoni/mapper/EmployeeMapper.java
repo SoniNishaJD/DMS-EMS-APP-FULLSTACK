96d13767f600fee07fbdb770be9911fd99dc4dba
Soni Nisha <soni.nishaa24@gmail.com>
2024-05-26 03:12:08 +0530

    first commit

---
+++ ems-backend/src/main/java/com/nishasoni/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.nishasoni.mapper;
+
+import com.nishasoni.dto.EmployeeDto;
+import com.nishasoni.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ ems-backend/src/test/java/com/nishasoni/FullstackemsdmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishasoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackemsdmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
