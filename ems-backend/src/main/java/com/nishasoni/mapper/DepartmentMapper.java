4ca3199e33c567ef41d7f32c0b3207e91adaf5c8
Soni Nisha <soni.nishaa24@gmail.com>
2024-05-27 18:03:54 +0530

    third Commit

---
+++ ems-backend/src/main/java/com/nishasoni/mapper/DepartmentMapper.java
@@ -0,0 +1,24 @@
+package com.nishasoni.mapper;
+
+import com.nishasoni.dto.DepartmentDto;
+import com.nishasoni.entity.Department;
+
+public class DepartmentMapper {
+
+    public static DepartmentDto mapToDepartmentDto(Department department){
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+    // convert department dto into department jpa entity
+    public static Department mapToDepartment(DepartmentDto departmentDto){
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription()
+        );
+    }
+}
---
+++ todo-management/src/main/java/com/nishasoni/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.nishasoni.repository;
+
+import com.nishasoni.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
