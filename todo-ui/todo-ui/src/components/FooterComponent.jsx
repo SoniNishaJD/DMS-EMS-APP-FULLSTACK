53a9a5da2167a77f3552028f77bd8cdc21b5305f
Soni Nisha <soni.nishaa24@gmail.com>
2024-06-13 14:58:35 +0530

    13 June

--- todo-ui/todo-ui/src/components/FooterComponent.jsx
+++ todo-ui/todo-ui/src/components/FooterComponent.jsx
@@ -3,7 +3,7 @@ const FooterComponent = () => {
   return (
     <div>
         <footer className='footer'>
-            <p className='text-center'>Copyrights reserved at 2023-25 by Java Guides</p>
+            <p className='text-center'>Copyrights reserved at 2023-25 by Nisha</p>
         </footer>
     </div>
   )
